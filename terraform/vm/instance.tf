de4a9d7da596bddb50c507909187cf633c12f9b4
Anil517 <53336408+Anil517@users.noreply.github.com>
2020-01-06 16:33:24 +0530

    Update instance.tf

--- terraform/vm/instance.tf
+++ terraform/vm/instance.tf
@@ -1,20 +1,37 @@
-resource "google_compute_instance" "vm_instance"{
-   name = "tset" 
-   machine_type= "${var.machine}"
-      zone = "${var.zone}" 
-   
-   boot_disk {
+resource "google_compute_instance" "finalvm" {
+  min_cpu_platform = var.cpu_platform
+  deletion_protection = var.deletion_protection
+  machine_type = var.machine_type
+  name = var.instance_name
+  zone = var.instance_zone
+  tags = var.tags
+  boot_disk {
+    auto_delete = var.auto_delete
+    mode = var.mode
     initialize_params {
-      image = "${var.image}"
-    }
-}
-
- network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral IP
+      image = var.image
+      size = var.size
+      type = var.type
     }
   }
+  service_account {
+    email = var.email
+    scopes = var.service_account_scopes
+  }
+ # metadata_startup_script = file(var.startup_script)
+
+  network_interface {
+    network = "${google_compute_network.v_network.name}"
+    subnetwork = "${google_compute_subnetwork.sub_network.name}"
+   # network_ip = var.network_ip
+    access_config {
+    }
+    #alias_ip_range {
+     #   subnetwork_range_name = var.subnetwork_range_name
+     #   ip_cidr_range  = var.ip_cidr_range
+    #}
+
+  }
 }
+
 
